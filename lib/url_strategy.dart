61a7f7f0a70b4099d965d7b56059e1c5d42eefad
Pierre Fld <fellendael.pierre@hotmail.com>
2024-08-19 20:56:30 +0200

    Update url_strategy.dart
    if (dart.library.web) => if (dart.library.js_interop)

--- lib/url_strategy.dart
+++ lib/url_strategy.dart
@@ -1,4 +1,11 @@
 
 
 //export 'url_strategy_noop.dart' if (kIsWeb) 'url_strategy_web.dart';
-export 'url_strategy_noop.dart' if (dart.library.html) 'url_strategy_web.dart';
+//export 'url_strategy_noop.dart' if (dart.library.html) 'url_strategy_web.dart';
+
+
+// https://dart.dev/libraries/dart-html
+// https://dart.dev/interop/js-interop/package-web
+export 'url_strategy_noop.dart'
+//if (dart.library.io) 'url_strategy_noop.dart'
+if (dart.library.js_interop) 'url_strategy_web.dart';
