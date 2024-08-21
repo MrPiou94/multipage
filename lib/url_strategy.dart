a28f7c1421fc7f0b64f8c66c8ea306f085cf14f6
Pierre Fld <fellendael.pierre@hotmail.com>
2024-08-21 15:02:33 +0200

    Update url_strategy.dart
    Compilation For v3.22.3

--- lib/url_strategy.dart
+++ lib/url_strategy.dart
@@ -1,11 +1,15 @@
 
 
-//export 'url_strategy_noop.dart' if (kIsWeb) 'url_strategy_web.dart';
-//export 'url_strategy_noop.dart' if (dart.library.html) 'url_strategy_web.dart';
+// OK VERSION 3.22.3
+export 'url_strategy_noop.dart' if (dart.library.html) 'url_strategy_web.dart';
+
+// ERROR VERSION 3.24.0
+//export 'url_strategy_noop.dart'
+//if (dart.library.js_interop) 'url_strategy_web.dart';
+
+
 
 
 // https://dart.dev/libraries/dart-html
 // https://dart.dev/interop/js-interop/package-web
-export 'url_strategy_noop.dart'
 //if (dart.library.io) 'url_strategy_noop.dart'
-if (dart.library.js_interop) 'url_strategy_web.dart';
